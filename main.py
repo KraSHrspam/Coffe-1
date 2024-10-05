6715617480fa47650333e0515cab2376496541d6
.c6 <mlevin0210@gmail.com>
2024-10-05 16:43:15 +0300

    Удалил лишние функции\ переименовал переменные

--- main.py
+++ main.py
@@ -17,7 +17,26 @@ def fetch_coordinates(apikey, place):
     return lon, lat
 
 
-def get_coffee_houses_with_distance(coffee_houses):
+def get_coffee_distance(coffee_coordinates):
+    return coffee_coordinates['Distance']
+
+
+def read_file():
+    with open('index.html') as file:
+        return file.read()
+
+
+def main():
+    with open('coffee.json', 'r', encoding='CP1251') as coffee:
+        coffee_file = coffee.read()
+    coffee_houses = json.loads(coffee_file)
+    yandex_apikey = '70c8480a-feed-4906-8c79-0d6744fe1cf5'
+
+    your_place = input("Ваше местоположение - ")
+    user_coords = fetch_coordinates(yandex_apikey, your_place)
+
+    user_latitude, user_longitude = user_coords
+
     cofe_coords = []
     for coffee_house in coffee_houses:
         latitude, longitude = coffee_house["geoData"]["coordinates"]
@@ -28,42 +47,13 @@ def get_coffee_houses_with_distance(coffee_houses):
             'Distance': distance.distance((latitude, longitude), (user_latitude, user_longitude)).km,
             }
         cofe_coords.append(coffe_coordinates)
-        return cofe_coords
 
-
-def get_coffee_distance(coffee_coordinates):
-    return coffee_coordinates['Distance']
-
-
-def get_nearest_coffee_houses(coffee_house_coordinates):
-    sorted_coffee = sorted(coffee_house_coordinates, key=get_coffee_distance)
-    nearest_coffee_houses = sorted_coffee[:coffee_in_page]
-    return nearest_coffee_houses
-
-
-def read_file():
-    with open('index.html') as file:
-        return file.read()
-
-
-def main():
-    with open(coffee_path, 'r', encoding=encode) as coffee:
-        coffee_file = coffee.read()
-    coffee_houses = json.loads(coffee_file)
-    apikey_of_user_coordinates = os.environ['API_KEY']
-
-    your_place = input("Ваше местоположение - ")
-    user_coords = fetch_coordinates(apikey_of_user_coordinates, your_place)
-
-    user_latitude, user_longitude = user_coords
-
-    coffee_house_coordinates = get_coffee_houses_with_distance(coffee_houses)
-
-    sorted_coffee_houses = get_nearest_coffee_houses(coffee_house_coordinates)
+    sorted_coffee = sorted(cofe_coords, key=get_coffee_distance)
+    nearest_coffee_houses = sorted_coffee[:5]
 
     coffee_map = folium.Map(location=[user_longitude, user_latitude])
 
-    for one_coffee_house in sorted_coffee_houses:
+    for one_coffee_house in nearest_coffee_houses:
         folium.Marker(
             [one_coffee_house['Longitude'], one_coffee_house['Latitude']],
             popup=one_coffee_house['title'], tooltip='').add_to(coffee_map)
